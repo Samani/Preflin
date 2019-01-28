15c9499b5ed05317ab4ff0cef6f2073c79e526ac
rasool <rasoul.samani@gmail.com>
2019-01-28 13:09:52 +0330

    serialize object with Base64

--- preflin/src/main/java/com/rsamani/preflin/DefaultSerializer.java
+++ preflin/src/main/java/com/rsamani/preflin/DefaultSerializer.java
@@ -1,11 +1,11 @@
 package com.rsamani.preflin;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.type.CollectionType;
+import android.util.Base64;
 
-import java.util.ArrayList;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.List;
 
 /**
@@ -16,9 +16,11 @@ public class DefaultSerializer implements Serializer {
     @Override
     public String toString(Object o) {
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-            return mapper.writeValueAsString(o);
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            ObjectOutputStream so = new ObjectOutputStream(bo);
+            so.writeObject(o);
+            so.flush();
+            return Base64.encodeToString(bo.toByteArray(), Base64.DEFAULT);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
@@ -28,8 +30,17 @@ public class DefaultSerializer implements Serializer {
     @Override
     public <T> T objectFromString(String s, Class<T> tClass) {
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(s, tClass);
+            byte b[] = Base64.decode(s.getBytes(), Base64.DEFAULT);
+            ByteArrayInputStream bi = new ByteArrayInputStream(b);
+            ObjectInputStream si = new ObjectInputStream(bi);
+            Object o = si.readObject();
+
+            if (o instanceof List) {
+                List tList = (List) o;
+                return tClass.cast(tList.toArray());
+            }
+
+            return tClass.cast(o);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
@@ -39,9 +50,12 @@ public class DefaultSerializer implements Serializer {
     @Override
     public <T> List<T> listObjectFromString(String s, Class<T> tClass) {
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            CollectionType listType = mapper.getTypeFactory().constructCollectionType(List.class, tClass);
-            return mapper.readValue(s, listType);
+            byte b[] = Base64.decode(s.getBytes(), Base64.DEFAULT);
+            ByteArrayInputStream bi = new ByteArrayInputStream(b);
+            ObjectInputStream si = new ObjectInputStream(bi);
+            Object o = si.readObject();
+            Class<List<T>> clazz = (Class) List.class;
+            return clazz.cast(o);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
